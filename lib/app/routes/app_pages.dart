6018a47f1e2fe7891fbc88ebb9a570f8b2d62e1e
Martín Silva Molina <msilvamolina@gmail.com>
2024-09-11 17:58:38 +0200

    .

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -21,7 +21,7 @@ class AppPages {
     ),
     GetPage(
       name: _Paths.LOGIN,
-      page: () => const LoginView(),
+      page: () => LoginView(),
       binding: LoginBinding(),
     ),
   ];
