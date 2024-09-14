89abc8d00fc1b52e82d0948a4365549525b739b7
Martín Silva Molina <msilvamolina@gmail.com>
2024-09-14 04:03:17 +0200

    .

--- lib/app/themes/theme.dart
+++ lib/app/themes/theme.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class Themes {
   final lightTheme = ThemeData(
     colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.green,
+      seedColor: Colors.greenAccent,
       brightness: Brightness.light,
     ),
     useMaterial3: true,
