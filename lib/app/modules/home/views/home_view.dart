6c2bc212b985daa7f0107a9a42e0e38c954ef9da
Martín Silva Molina <msilvamolina@gmail.com>
2024-09-14 05:36:38 +0200

    .

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -81,107 +81,108 @@ class _HomeViewState extends State<HomeView> {
           ),
           body: Row(
             children: [
-              Stack(
-                children: [
-                  NavigationRail(
-                    useIndicator: true,
-                    extended: isTablet ? false : _navigationExtended,
-                    selectedIndex: _selectedIndex,
-                    onDestinationSelected: (int index) {
-                      setState(() {
-                        _selectedIndex = index;
-                      });
-                    },
-                    destinations: navigationDestinations,
-                  ),
-                  GetBuilder<HomeController>(
-                    id: 'themeButton',
-                    builder: (_) {
-                      return Positioned(
-                        bottom: 16,
-                        left: 16,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            if (isTablet ? false : _navigationExtended) ...[
-                              Container(
-                                width: 160,
-                                child: Text(
-                                  'Log in to follow creators, like videos, and view comments.',
-                                  style: TypographyStyle.bodyRegularSmall,
+              if (!isMobile)
+                Stack(
+                  children: [
+                    NavigationRail(
+                      useIndicator: true,
+                      extended: isTablet ? false : _navigationExtended,
+                      selectedIndex: _selectedIndex,
+                      onDestinationSelected: (int index) {
+                        setState(() {
+                          _selectedIndex = index;
+                        });
+                      },
+                      destinations: navigationDestinations,
+                    ),
+                    GetBuilder<HomeController>(
+                      id: 'themeButton',
+                      builder: (_) {
+                        return Positioned(
+                          bottom: 16,
+                          left: 16,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              if (isTablet ? false : _navigationExtended) ...[
+                                Container(
+                                  width: 160,
+                                  child: Text(
+                                    'Log in to follow creators, like videos, and view comments.',
+                                    style: TypographyStyle.bodyRegularSmall,
+                                  ),
                                 ),
-                              ),
-                              SizedBox(height: 16),
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text('Ingresar'),
-                              ),
-                              SizedBox(height: 16),
-                              Container(
-                                decoration: BoxDecoration(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .tertiaryContainer,
-                                    borderRadius: BorderRadius.circular(16)),
-                                padding: EdgeInsets.all(16),
-                                child: Row(
-                                  children: [
-                                    Icon(
-                                      Icons.emoji_objects,
-                                      size: 22,
-                                    ),
-                                    SizedBox(width: 8),
-                                    Text(
-                                      'Creá un efecto Estrellas',
-                                      style: TypographyStyle.bodyRegularSmall
-                                          .copyWith(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .tertiary,
+                                SizedBox(height: 16),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text('Ingresar'),
+                                ),
+                                SizedBox(height: 16),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .tertiaryContainer,
+                                      borderRadius: BorderRadius.circular(16)),
+                                  padding: EdgeInsets.all(16),
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.emoji_objects,
+                                        size: 22,
                                       ),
-                                    ),
-                                  ],
+                                      SizedBox(width: 8),
+                                      Text(
+                                        'Creá un efecto Estrellas',
+                                        style: TypographyStyle.bodyRegularSmall
+                                            .copyWith(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .tertiary,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(height: 16),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Text('Company'),
+                                ),
+                                SizedBox(height: 16),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Text('Program'),
+                                ),
+                                SizedBox(height: 16),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Text('Terms & Policies'),
+                                ),
+                                SizedBox(height: 16),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    '© 2024 Estrellas',
+                                    style: TypographyStyle.bodyBlackSmall,
+                                  ),
+                                ),
+                                SizedBox(height: 26),
+                              ],
+                              IconButton.outlined(
+                                onPressed: controller.changeThemeMode,
+                                icon: Padding(
+                                  padding: const EdgeInsets.all(4),
+                                  child: Icon(controller.getThemeIcon()),
                                 ),
                               ),
-                              SizedBox(height: 16),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text('Company'),
-                              ),
-                              SizedBox(height: 16),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text('Program'),
-                              ),
-                              SizedBox(height: 16),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text('Terms & Policies'),
-                              ),
-                              SizedBox(height: 16),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text(
-                                  '© 2024 Estrellas',
-                                  style: TypographyStyle.bodyBlackSmall,
-                                ),
-                              ),
-                              SizedBox(height: 26),
                             ],
-                            IconButton.outlined(
-                              onPressed: controller.changeThemeMode,
-                              icon: Padding(
-                                padding: const EdgeInsets.all(4),
-                                child: Icon(controller.getThemeIcon()),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               Expanded(
                 child: Center(
                   child: Container(
