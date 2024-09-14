d42d76b392cbc65e32064a7df4ede65d6022572b
Martín Silva Molina <msilvamolina@gmail.com>
2024-09-15 00:46:14 +0200

    new video model

--- lib/app/modules/home/views/components/video.dart
+++ lib/app/modules/home/views/components/video.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_estrellas/app/data/models/video_model.dart';
 import 'package:video_player/video_player.dart';
 
 /// Stateful widget to fetch and then display video content.
 class VideoApp extends StatefulWidget {
-  const VideoApp({super.key, required this.videoUrl});
+  const VideoApp({super.key, required this.videoModel});
 
-  final String videoUrl;
+  final VideoModel videoModel;
   @override
   _VideoAppState createState() => _VideoAppState();
 }
@@ -16,13 +17,14 @@ class _VideoAppState extends State<VideoApp> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl))
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {
-          _controller.play();
-        });
-      });
+    _controller =
+        VideoPlayerController.networkUrl(Uri.parse(widget.videoModel.videoUrl))
+          ..initialize().then((_) {
+            // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+            setState(() {
+              _controller.play();
+            });
+          });
   }
 
   @override
