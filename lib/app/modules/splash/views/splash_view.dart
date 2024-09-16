d91cc58d05d44523cb336c81862598fd3d360580
Martín Silva Molina <msilvamolina@gmail.com>
2024-09-16 09:41:09 +0200

    feat/login_ios

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_estrellas/app/components/dialogs/loader_dialog.dart';
 import 'package:flutter_estrellas/app/components/layouts/auth_layout.dart';
 import 'package:flutter_estrellas/app/routes/app_pages.dart';
 import 'package:flutter_svg/svg.dart';
@@ -30,22 +31,6 @@ class _SplashViewState extends State<SplashView> {
 
   @override
   Widget build(BuildContext context) {
-    return AuthLayout(
-      showBackground: false,
-      child: Center(
-        child: AnimatedScale(
-          curve: Curves.bounceOut,
-          scale: logoInitialScale ? 0 : 1,
-          duration: Duration(seconds: 2),
-          child: Hero(
-            tag: 'logo',
-            child: SvgPicture.asset(
-              'assets/svg/logo.svg',
-              width: 100,
-            ),
-          ),
-        ),
-      ),
-    );
+    return LoaderDialog();
   }
 }
