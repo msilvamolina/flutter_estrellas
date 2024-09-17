df04345c878627ce5a307dca8a4b6f62aaba8b23
Martín Silva Molina <msilvamolina@gmail.com>
2024-09-17 06:23:57 +0200

    product animation done

--- lib/app/modules/product/views/product_view.dart
+++ lib/app/modules/product/views/product_view.dart
@@ -1,10 +1,12 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_estrellas/app/themes/styles/typography.dart';
 
 import 'package:get/get.dart';
 
 import '../../../components/layouts/dialog_layout.dart';
+import '../../../themes/styles/colors.dart';
 import '../controllers/product_controller.dart';
 
 class ProductView extends GetView<ProductController> {
@@ -13,7 +15,63 @@ class ProductView extends GetView<ProductController> {
   Widget build(BuildContext context) {
     return DialogLayout(
       child: Scaffold(
-        body: Text('peps'),
+        body: NestedScrollView(
+          headerSliverBuilder:
+              (BuildContext context, bool innerBoxIsScrolled) => <Widget>[
+            SliverOverlapAbsorber(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                context,
+              ),
+              sliver: SliverAppBar(
+                expandedHeight: 510,
+                centerTitle: true,
+                pinned: true,
+                elevation: 0,
+                forceElevated: innerBoxIsScrolled,
+                leading:
+                    IconButton(onPressed: Get.back, icon: Icon(Icons.close)),
+                flexibleSpace: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    // Determinar si la barra está colapsada
+                    final double collapseOffset = constraints.biggest.height;
+                    final bool isCollapsed =
+                        collapseOffset <= (kToolbarHeight + 60);
+
+                    return FlexibleSpaceBar(
+                      centerTitle: true,
+                      title: isCollapsed
+                          ? Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 46, right: 16),
+                              child: Text(
+                                'Bolso femenino cuero y tiras metálicas Bolso femenino cuero y tiras metálicas',
+                                style: TypographyStyle.bodyBlackLarge,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            )
+                          : null, // Solo mostrar título cuando esté colapsado
+                      background: Image.asset(
+                        'assets/images/product.png',
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+          body: Text('asdad'),
+          // SingleChildScrollView(
+          //   child: Column(
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     mainAxisAlignment: MainAxisAlignment.start,
+          //     children: [
+          //       Image.asset('assets/images/product.png'),
+          //       Text('peps'),
+          //     ],
+          //   ),
+        ),
       ),
     );
   }
