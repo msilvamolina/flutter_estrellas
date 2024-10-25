7c7562601b16a37f1ec94ae1cca4b1ef21973d0a
Martín Silva Molina <msilvamolina@gmail.com>
2024-10-25 22:45:49 +0200

    user catalog

---
+++ lib/app/data/models/user_catalog/user_catalog_model.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../helpers/model_helpers.dart';
+import '../product_firebase_lite/product_firebase_lite.dart';
+
+part 'user_catalog_model.freezed.dart';
+part 'user_catalog_model.g.dart';
+
+@freezed
+abstract class UserCatalogModel implements _$UserCatalogModel {
+  const UserCatalogModel._();
+
+  const factory UserCatalogModel({
+    required String id,
+    required String name,
+    required String createdBy,
+    required String createdByUserId,
+    List<ProductFirebaseLiteModel>? products,
+  }) = _UserCatalogModel;
+
+  factory UserCatalogModel.fromJson(Map<String, dynamic> json) =>
+      _$UserCatalogModelFromJson(json);
+
+  factory UserCatalogModel.fromDocument(DocumentSnapshot doc) =>
+      UserCatalogModel.fromJson(ModelHelpers.fromDocument(doc.data()!));
+
+  Map<String, dynamic> toDocument() => ModelHelpers.toDocument(toJson());
+}
