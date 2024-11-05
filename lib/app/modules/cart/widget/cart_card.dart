1d524c45707e1c94650a647c7a471b0081c30cba
Martín Silva Molina <msilvamolina@gmail.com>
2024-11-05 17:53:36 +0100

    new card in cart

--- lib/app/modules/cart/widget/cart_card.dart
+++ lib/app/modules/cart/widget/cart_card.dart
@@ -11,52 +11,51 @@ class CartCard extends StatelessWidget {
   final UserProductModel userProductModel;
   @override
   Widget build(BuildContext context) {
-    return Container();
-    // ProductFirebaseLiteModel product = userProductModel.product!;
-    // return Padding(
-    //   padding: const EdgeInsets.all(16),
-    //   child: Row(
-    //     crossAxisAlignment: CrossAxisAlignment.start,
-    //     mainAxisAlignment: MainAxisAlignment.start,
-    //     children: [
-    //       ClipOval(
-    //         child: Image.network(
-    //           product.thumbnail ?? '',
-    //           width: 40,
-    //           height: 40,
-    //           fit: BoxFit.cover,
-    //         ),
-    //       ),
-    //       SizedBox(width: 8),
-    //       Expanded(
-    //         child: Row(
-    //           children: [
-    //             Expanded(
-    //               child: Column(
-    //                 crossAxisAlignment: CrossAxisAlignment.start,
-    //                 mainAxisAlignment: MainAxisAlignment.start,
-    //                 children: [
-    //                   Text(
-    //                     product.name ?? '',
-    //                     style: TypographyStyle.bodyRegularMedium,
-    //                     maxLines: 1,
-    //                     overflow: TextOverflow.ellipsis,
-    //                   ),
-    //                   SizedBox(height: 4),
-    //                 ],
-    //               ),
-    //             ),
-    //             Text(
-    //               product.suggestedPrice.toString(),
-    //               style: TypographyStyle.bodyBlackLarge,
-    //               maxLines: 1,
-    //               overflow: TextOverflow.ellipsis,
-    //             ),
-    //           ],
-    //         ),
-    //       ),
-    //     ],
-    //   ),
-    // );
+    ProductFirebaseLiteModel product = userProductModel.video!.product!;
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ClipOval(
+            child: Image.network(
+              product.thumbnail ?? '',
+              width: 40,
+              height: 40,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(width: 8),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        product.name ?? '',
+                        style: TypographyStyle.bodyRegularMedium,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      SizedBox(height: 4),
+                    ],
+                  ),
+                ),
+                Text(
+                  product.suggestedPrice.toString(),
+                  style: TypographyStyle.bodyBlackLarge,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
