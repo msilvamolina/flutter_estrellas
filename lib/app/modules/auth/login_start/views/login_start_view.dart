4f5075c2e3a6c91267b448549b24717789fdf41e
Martín Silva Molina <msilvamolina@gmail.com>
2024-11-14 19:37:38 +0100

    nueva pantalla login

--- lib/app/modules/auth/login_start/views/login_start_view.dart
+++ lib/app/modules/auth/login_start/views/login_start_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_estrellas/app/libraries/icons/icons_font.dart';
+import 'package:flutter_estrellas/app/themes/styles/typography.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../themes/styles/colors.dart';
 import '../controllers/login_start_controller.dart';
 
 class LoginStartView extends GetView<LoginStartController> {
@@ -9,14 +12,39 @@ class LoginStartView extends GetView<LoginStartController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('LoginStartView'),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Text(''),
         centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: GestureDetector(
+              onTap: () {},
+              child: Row(
+                children: [
+                  Text(
+                    'Ayuda',
+                    style: TypographyStyle.bodyRegularLarge,
+                  ),
+                  Icon(
+                    EstrellasIcons.question,
+                    size: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'LoginStartView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        color: white,
+        child: Column(
+          children: [
+            Image.asset('assets/images/login-background.png'),
+          ],
         ),
       ),
     );
