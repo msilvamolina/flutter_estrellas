921f085d22148376ba56b4284779fb68ca5ee4ac
Martín Silva Molina <msilvamolina@gmail.com>
2024-11-19 07:43:21 +0100

    feat/new_iphone_fixes_firebase_new

--- lib/app/services/dependency_injection.dart
+++ lib/app/services/dependency_injection.dart
@@ -1,32 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-// import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 import '../app/controllers/main_controller.dart';
 import '../app/controllers/user_product_controller.dart';
-import '../app/layout/remote_config/remote_config_controller.dart';
 import '../data/providers/local/local_storage.dart';
 
 class DependecyInjection {
-  static void init() async {
-    try {
-      Get.put<GetStorage>(GetStorage());
-      Get.put<LocalStorage>(LocalStorage());
-      // Get.put<FirebaseRemoteConfig>(FirebaseRemoteConfig.instance);
-      Get.put<FirebaseFirestore>(FirebaseFirestore.instance);
-      Get.put<FirebaseStorage>(FirebaseStorage.instance);
-      Get.put<MainController>(MainController());
-      // Get.put(RemoteConfigController());
-      Get.put<UserProductController>(UserProductController());
-      print("Dependencias inicializadas correctamente");
-    } catch (e, stackTrace) {
-      print("Error inicializando dependencias: $e");
-      print(stackTrace);
-      FirebaseCrashlytics.instance.recordError(e, stackTrace,
-          reason: "Error en DependecyInjection.init");
-    }
+  static void init() {
+    Get.put<GetStorage>(GetStorage());
+    Get.put<LocalStorage>(LocalStorage());
+    Get.put<FirebaseFirestore>(FirebaseFirestore.instance);
+    Get.put<FirebaseStorage>(FirebaseStorage.instance);
+    Get.put<MainController>(MainController());
+    Get.put<UserProductController>(UserProductController());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,29 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-// import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:reactive_phone_form_field/reactive_phone_form_field.dart';
-
-import 'app/app/layout/remote_config/remote_config_layout.dart';
 import 'app/config/firebase_config.dart';
 import 'app/routes/app_pages.dart';
 import 'app/services/dependency_injection.dart';
-import 'app/services/environment.dart';
+import 'app/services/theme_service.dart';
 import 'app/themes/text_theme.dart';
 import 'app/themes/theme.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
-  const String flavor = String.fromEnvironment('flavor', defaultValue: '');
-
-  await Environment.instance.init(env: flavor);
-
-  // Inicializar Firebase y Remote Config
   await initFirebase();
-  // await initRemoteConfig();
 
-  // Inicializar dependencias
   DependecyInjection.init();
 
-  // Manejo global de errores
   runZonedGuarded(
     () {
-      runApp(const MyApp());
+      runApp(
+        const MyApp(),
+      );
     },
     (Object error, StackTrace stackTrace) {
       FirebaseCrashlytics.instance.recordError(error, stackTrace);
@@ -43,7 +33,6 @@ void main() async {
   );
 }
 
-/// Inicialización de Firebase
 Future<void> initFirebase() async {
   FirebaseOptions? firebaseOptions;
   if (kIsWeb) {
@@ -71,40 +60,9 @@ Future<void> initFirebase() async {
       trackingId: firebaseConfig['trackingId'],
       storageBucket: firebaseConfig['storageBucket'],
     );
-  } else if (Platform.isIOS) {
-    // // Usar el archivo GoogleService-Info.plist para iOS
-    // await Firebase.initializeApp();
-
-    Map<String, dynamic>? firebaseConfig = firebaseConfigDevIOS;
-    firebaseOptions = FirebaseOptions(
-      apiKey: firebaseConfig['apiKey']!,
-      appId: firebaseConfig['appId']!,
-      messagingSenderId: firebaseConfig['messagingSenderId']!,
-      projectId: firebaseConfig['projectId']!,
-      databaseURL: firebaseConfig['databaseURL'],
-      measurementId: firebaseConfig['measurementId'],
-      trackingId: firebaseConfig['trackingId'],
-      storageBucket: firebaseConfig['storageBucket'],
-    );
   }
-
   await Firebase.initializeApp(options: firebaseOptions);
 }
-
-/// Inicialización de Firebase Remote Config
-// Future<void> initRemoteConfig() async {
-//   final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
-
-//   try {
-//     await remoteConfig.setDefaults({});
-//     await remoteConfig.fetchAndActivate();
-//     print('Remote Config inicializado correctamente.');
-//   } catch (e) {
-//     print('Error inicializando Remote Config: $e');
-//     FirebaseCrashlytics.instance.recordError(e, StackTrace.current,
-//         reason: "Error en initRemoteConfig");
-//   }
-// }
 
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
@@ -118,12 +76,7 @@ class MyApp extends StatelessWidget {
 
     return GetMaterialApp(
       navigatorObservers: [routeObserver],
-      localizationsDelegates: const [
-        ...PhoneFieldLocalization.delegates,
-      ],
-      supportedLocales: const [
-        Locale('es', ''),
-      ],
+
       title: "Estrellas",
       locale: const Locale('es'),
       fallbackLocale: const Locale('es'),
@@ -134,7 +87,7 @@ class MyApp extends StatelessWidget {
       // themeMode: ThemeService.getThemeMode(),
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-      // builder: (context, child) => RemoteConfigLayout(child: child!),
+      // builder: (context, child) => MainLayout(child: child!),
     );
   }
 }
