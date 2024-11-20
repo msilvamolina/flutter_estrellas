94c2d28483e3afc2f0d931c0fbebfebe025e0854
Martín Silva Molina <msilvamolina@gmail.com>
2024-11-20 17:21:14 +0100

    ya tenemos el diseno

--- lib/app/modules/account/profile/controllers/profile_controller.dart
+++ lib/app/modules/account/profile/controllers/profile_controller.dart
@@ -1,9 +1,53 @@
+import 'package:flutter_estrellas/app/app/controllers/main_controller.dart';
 import 'package:get/get.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+import 'package:reactive_phone_form_field/reactive_phone_form_field.dart';
+
+enum Fields {
+  fullname('fullname'),
+  document('document'),
+  phone('phone'),
+  email('email'),
+  ;
+
+  const Fields(this.text);
+  final String text;
+}
 
 class ProfileController extends GetxController {
-  //TODO: Implement ProfileController
+  MainController mainController = Get.find<MainController>();
+  FormGroup buildForm() => fb.group(<String, Object>{
+        Fields.fullname.name: FormControl<String>(
+          validators: [
+            Validators.required,
+            Validators.minLength(4),
+          ],
+        ),
+        Fields.email.name: FormControl<String>(
+          validators: [
+            Validators.required,
+            Validators.email,
+          ],
+        ),
+        Fields.document.name: FormControl<String>(
+          validators: [
+            Validators.required,
+            Validators.number(allowedDecimals: 0),
+            Validators.minLength(8),
+          ],
+        ),
+        Fields.phone.name: FormControl<PhoneNumber>(
+          value: const PhoneNumber(
+            isoCode: IsoCode.CO,
+            nsn: '',
+          ),
+          validators: [
+            PhoneValidators.required,
+            PhoneValidators.valid,
+          ],
+        ),
+      });
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
@@ -19,5 +63,35 @@ class ProfileController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  Future<void> sendForm(Map<String, Object?> data) async {
+    String fullname = data[Fields.fullname.name].toString();
+    String document = data[Fields.document.name].toString();
+    PhoneNumber phone = data[Fields.phone.name] as PhoneNumber;
+
+    mainController.showLoader(
+      title: 'Verificando dirección',
+    );
+
+    print('fullname $fullname');
+    print('document $document');
+    print('phone $phone');
+
+    // Either<String, AddressModel> response = await _repository.addAddress(
+    //   fullname: name,
+    //   city: _cityModel,
+    //   department: _departmentModel,
+    //   address: address,
+    //   phone: phone,
+    //   notes: notes,
+    //   save: saveAddress.value,
+    // );
+    // Get.back();
+    // response.fold((failure) {
+    //   Snackbars.error(failure);
+    // }, (AddressModel addressModel) async {
+    //   Snackbars.success('Dirección agregada correctamente');
+
+    //   Get.offNamed(Routes.SELECT_PAYMENT, arguments: addressModel);
+    // });
+  }
 }
--- lib/app/modules/account/profile/views/profile_view.dart
+++ lib/app/modules/account/profile/views/profile_view.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_estrellas/app/components/appbar/estrellas_appbar.dart';
+import 'package:flutter_estrellas/app/themes/styles/colors.dart';
+import 'package:flutter_estrellas/app/themes/styles/typography.dart';
 
 import 'package:get/get.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
+import '../../../../components/buttons/buttons.dart';
+import '../../../../components/inputs/phone_input.dart';
+import '../../../../components/inputs/text_input.dart';
 import '../controllers/profile_controller.dart';
 import '../widgets/photo_card_empty.dart';
 
@@ -10,22 +16,87 @@ class ProfileView extends GetView<ProfileController> {
   const ProfileView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: EstrellasAppbar(title: 'Editar perfil'),
-      body: Container(
-        padding: EdgeInsets.only(top: 94),
-        width: double.infinity,
-        child: Column(
-          children: [
-            PhotoCardEmpty(
-              isFull: true,
-              // imageUrl: 'assets/images/product.png',
+    return ReactiveFormBuilder(
+      form: controller.buildForm,
+      builder: (context, form, child) {
+        return Scaffold(
+          appBar: EstrellasAppbar(title: 'Editar perfil'),
+          bottomNavigationBar: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: ReactiveFormConsumer(
+                builder: (context, form, child) => Button(
+                  label: 'Guardar',
+                  style: ButtonStyles.primary,
+                  onPressed: (form.valid)
+                      ? () => controller.sendForm(form.value)
+                      : null,
+                ),
+              ),
             ),
-            Text('Andrea Sánchez'),
-            Text('50% completado'),
-          ],
-        ),
-      ),
+          ),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(top: 94),
+              width: double.infinity,
+              child: Column(
+                children: [
+                  PhotoCardEmpty(
+                    isFull: true,
+                    // imageUrl: 'assets/images/product.png',
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Andrea Sánchez',
+                    style: TypographyStyle.bodyBlackLarge,
+                  ),
+                  SizedBox(height: 2),
+                  Text(
+                    '50% completado',
+                    style: TypographyStyle.bodyRegularMedium
+                        .copyWith(color: secondaryBase),
+                  ),
+                  SizedBox(height: 46),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        CustomTextInput(
+                          autofocus: false,
+                          formControlName: Fields.fullname.name,
+                          keyboardType: TextInputType.text,
+                          label: 'Nombre completo',
+                        ),
+                        const SizedBox(height: 32),
+                        CustomTextInput(
+                          autofocus: false,
+                          formControlName: Fields.document.name,
+                          keyboardType: TextInputType.number,
+                          label: 'Número de documento',
+                        ),
+                        const SizedBox(height: 32),
+                        CustomTextInput(
+                          autofocus: false,
+                          formControlName: Fields.email.name,
+                          keyboardType: TextInputType.text,
+                          label: 'Correo',
+                        ),
+                        const SizedBox(height: 32),
+                        CustomPhoneInput(
+                          autofocus: false,
+                          formControlName: Fields.phone.name,
+                          keyboardType: TextInputType.phone,
+                          label: 'Celular',
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
