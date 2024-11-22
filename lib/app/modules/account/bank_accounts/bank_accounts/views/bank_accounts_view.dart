9dad88564e22d22c98b7836cdea82b25d70da16f
Martín Silva Molina <msilvamolina@gmail.com>
2024-11-22 08:02:36 +0100

    new bank account

--- lib/app/modules/account/bank_accounts/bank_accounts/views/bank_accounts_view.dart
+++ lib/app/modules/account/bank_accounts/bank_accounts/views/bank_accounts_view.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_estrellas/app/components/appbar/estrellas_appbar.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/bank_accounts_controller.dart';
+import '../widgets/bank_account_empty_state.dart';
 
 class BankAccountsView extends GetView<BankAccountsController> {
   const BankAccountsView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BankAccountsView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'BankAccountsView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      appBar: EstrellasAppbar(title: 'Cuentas bancarias'),
+      body: BankAccountEmptyState(),
     );
   }
 }
---
+++ lib/app/modules/account/bank_accounts/bank_accounts/widgets/bank_account_empty_state.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../../components/buttons/buttons.dart';
+import '../../../../../routes/app_pages.dart';
+import '../../../../../themes/styles/colors.dart';
+import '../../../../../themes/styles/typography.dart';
+
+class BankAccountEmptyState extends StatelessWidget {
+  const BankAccountEmptyState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String image = 'assets/images/catalog.png';
+    return Scaffold(
+      backgroundColor: white,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              right: -130,
+              top: 0,
+              child: Opacity(
+                opacity: 0.3,
+                child: Image.asset(
+                  image,
+                  width: 320,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Spacer(),
+                  Image.asset(
+                    image,
+                    width: 80,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'No tienes una cuenta agregada',
+                    style: TypographyStyle.bodyBlackLarge
+                        .copyWith(fontWeight: FontWeight.w600, fontSize: 20),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Agrega tu cuenta bancaria para empezar a recibir tus ganancias, ¡es rápido y fácil!',
+                    style: TypographyStyle.bodyRegularLarge.copyWith(),
+                  ),
+                  SizedBox(height: 22),
+                  Center(
+                    child: Button(
+                      label: 'Agregar nueva cuenta',
+                      onPressed: () {
+                        Get.toNamed(Routes.NEW_BANK_ACCOUNT);
+                      },
+                      style: ButtonStyles.secondaryLink,
+                    ),
+                  ),
+                  SizedBox(height: 22),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
