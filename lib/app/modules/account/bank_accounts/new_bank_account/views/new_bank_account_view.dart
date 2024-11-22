c98da9e06c71ec051d6e702fbb78ca4896a3dd91
Martín Silva Molina <msilvamolina@gmail.com>
2024-11-22 08:23:18 +0100

    ya tenemos la estructura

--- lib/app/modules/account/bank_accounts/new_bank_account/views/new_bank_account_view.dart
+++ lib/app/modules/account/bank_accounts/new_bank_account/views/new_bank_account_view.dart
@@ -7,6 +7,7 @@ import 'package:reactive_forms/reactive_forms.dart';
 import '../../../../../components/buttons/buttons.dart';
 import '../../../../../components/dropdown/dropdown.dart';
 import '../../../../../components/inputs/phone_input.dart';
+import '../../../../../components/inputs/radio_button_input.dart';
 import '../../../../../components/inputs/text_input.dart';
 import '../../../../../themes/styles/colors.dart';
 import '../../../../../themes/styles/typography.dart';
@@ -31,8 +32,6 @@ class NewBankAccountView extends GetView<NewBankAccountController> {
                         label: 'Continuar',
                         style: ButtonStyles.primary,
                         onPressed: () {},
-                        // onPressed:
-                        //     (form.valid) ? () => controller.sendForm(form.value) : null,
                       ),
                     ),
                   ),
@@ -49,156 +48,47 @@ class NewBankAccountView extends GetView<NewBankAccountController> {
                             autofocus: false,
                             formControlName: Fields.name.name,
                             keyboardType: TextInputType.text,
-                            label: 'Nombre completo',
-                            hintText: 'Como está en tu documento',
+                            label: 'Número de cuenta',
+                            hintText: 'Ingresa el número',
                           ),
                           const SizedBox(height: 22),
-                          CustomPhoneInput(
-                            autofocus: false,
-                            formControlName: Fields.phone.name,
-                            keyboardType: TextInputType.phone,
-                            label: 'Celular',
-                            hintText: 'Ingresa el número de celular',
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 18, vertical: 8),
-                            child: Text(
-                              'Te llamaremos si tenemos problemas en el envío',
-                            ),
-                          ),
-                          const SizedBox(height: 16),
-                          Column(
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              ReactiveRadioListTile<String>(
-                                formControlName: Fields.accountType.name,
-                                title: const Text('Cuenta Corriente'),
-                                value: 'current',
+                              Expanded(
+                                child: RadioButtonInput(
+                                  formControlName: Fields.accountType.name,
+                                  value: 'savings',
+                                  text: 'Ahorros',
+                                ),
                               ),
-                              ReactiveRadioListTile<String>(
-                                formControlName: Fields.accountType.name,
-                                title: const Text('Cuenta de Ahorros'),
-                                value: 'savings',
+                              SizedBox(width: 16),
+                              Expanded(
+                                child: RadioButtonInput(
+                                  formControlName: Fields.accountType.name,
+                                  value: 'current',
+                                  text: 'Corriente',
+                                ),
                               ),
                             ],
                           ),
-                          const SizedBox(height: 16),
-                          Text(
-                            'Lugar de envío',
-                            style: TypographyStyle.bodyBlackLarge2,
-                          ),
-                          const SizedBox(height: 16),
-                          // Obx(
-                          //   () => DropDown(
-                          //     label: 'Departamento',
-                          //     error: controller.departmentError.value,
-                          //     selectedValue:
-                          //         controller.departmentSelected.value,
-                          //     values: controller.departmentsList
-                          //         .map(
-                          //           (DepartmentModel value) => OptionDropDown(
-                          //             text: value.name,
-                          //             value: value.dropiId.toString(),
-                          //           ),
-                          //         )
-                          //         .toList(),
-                          //     onChanged: controller.onDepartmentSelected,
-                          //   ),
-                          // ),
-                          const SizedBox(height: 16),
-                          // Obx(
-                          //   () => controller.cityList.isNotEmpty
-                          //       ? DropDown(
-                          //           label: 'Ciudad o municipio',
-                          //           error: controller.cityError.value,
-                          //           selectedValue:
-                          //               controller.citySelected.value,
-                          //           values: controller.cityList
-                          //               .map(
-                          //                 (CityModel value) => OptionDropDown(
-                          //                   text: value.name ?? '',
-                          //                   value: value.id.toString(),
-                          //                 ),
-                          //               )
-                          //               .toList(),
-                          //           onChanged: controller.onCitySelected,
-                          //         )
-                          //       : SizedBox.shrink(),
-                          // ),
-                          const SizedBox(height: 26),
+                          const SizedBox(height: 32),
                           CustomTextInput(
                             autofocus: false,
-                            formControlName: Fields.address.name,
+                            formControlName: Fields.name.name,
                             keyboardType: TextInputType.text,
-                            label: 'Dirección',
-                            hintText: 'Ingresa la dirección',
+                            label: 'Nombre del titular',
+                            hintText: 'Ingresa el nombre',
                           ),
-                          const SizedBox(height: 10),
-                          // Obx(
-                          //   () => InkWell(
-                          //     onTap: (){},
-                          //     child: Padding(
-                          //       padding:
-                          //           const EdgeInsets.symmetric(vertical: 16),
-                          //       child: Row(
-                          //         children: [
-                          //           SvgPicture.asset(
-                          //             controller.saveAddress.value
-                          //                 ? 'assets/svg/CheckboxActive.svg'
-                          //                 : 'assets/svg/Checkbox.svg',
-                          //             width: 20,
-                          //           ),
-                          //           SizedBox(width: 10),
-                          //           Text(
-                          //             'Guardar dirección',
-                          //             style: TypographyStyle.bodyRegularLarge,
-                          //           ),
-                          //         ],
-                          //       ),
-                          //     ),
-                          //   ),
-                          // ),
-                          const SizedBox(height: 16),
+                          const SizedBox(height: 32),
                           CustomTextInput(
                             autofocus: false,
-                            maxLines: 6,
-                            formControlName: Fields.notes.name,
+                            formControlName: Fields.name.name,
                             keyboardType: TextInputType.text,
-                            label: 'Referencias adicionales',
-                            hintText:
-                                'Ingresa una descripción de la dirección o indicaciones de entrega',
+                            label: 'Número de documento',
+                            hintText: 'Ingresa el número',
                           ),
-                          const SizedBox(height: 16),
-                          RichText(
-                            textAlign: TextAlign.start,
-                            text: TextSpan(
-                              text: 'Al continuar, aceptas los ',
-                              style: TypographyStyle.bodyRegularMedium
-                                  .copyWith(color: neutral800),
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text: 'Términos y condiciones',
-                                  style: TypographyStyle.bodyRegularMedium
-                                      .copyWith(color: secondaryBase),
-                                ),
-                                TextSpan(
-                                  text: ' y la ',
-                                  style: TypographyStyle.bodyRegularMedium
-                                      .copyWith(color: neutral800),
-                                ),
-                                TextSpan(
-                                  text: 'Política de privacidad',
-                                  style: TypographyStyle.bodyRegularMedium
-                                      .copyWith(color: secondaryBase),
-                                ),
-                                TextSpan(
-                                  text: ' de Estrellas.',
-                                  style: TypographyStyle.bodyRegularMedium
-                                      .copyWith(color: neutral800),
-                                ),
-                              ],
-                            ),
-                          ),
+                          const SizedBox(height: 22),
                           const SizedBox(height: 26),
                         ],
                       ),
