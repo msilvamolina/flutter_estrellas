79b179b4b4f5bc60abdea82ea8171811e1d1cebf
Martín Silva Molina <msilvamolina@gmail.com>
2024-11-22 09:21:09 +0100

    ya guardamos la cuenta bancaria

--- lib/app/modules/account/bank_accounts/new_bank_confirmation/controllers/new_bank_confirmation_controller.dart
+++ lib/app/modules/account/bank_accounts/new_bank_confirmation/controllers/new_bank_confirmation_controller.dart
@@ -1,7 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_estrellas/app/app/controllers/main_controller.dart';
 import 'package:flutter_estrellas/app/data/models/bank_account/bank_account_model.dart';
+import 'package:flutter_estrellas/app/data/providers/repositories/bank_accounts/bank_accounts.dart';
 import 'package:get/get.dart';
 
+import '../../../../../components/snackbars/snackbars.dart';
+import '../../../../../data/models/address/address_model.dart';
+
 class NewBankConfirmationController extends GetxController {
+  MainController mainController = Get.find<MainController>();
+  BankAccountsRepository _repository = BankAccountsRepository();
+
   late BankAccountModel bankAccountModel;
 
   @override
@@ -10,5 +19,20 @@ class NewBankConfirmationController extends GetxController {
     super.onInit();
   }
 
-  void save() {}
+  Future<void> save() async {
+    mainController.showLoader(
+      title: 'Guardando cuenta bancaria',
+    );
+
+    Either<String, Unit> response =
+        await _repository.addBankAccount(bankAccount: bankAccountModel);
+    Get.back();
+    response.fold((failure) {
+      Snackbars.error(failure);
+    }, (_) async {
+      Get.back();
+      Get.back();
+      Snackbars.success('Tu cuenta bancaria se agregó exitosamente');
+    });
+  }
 }
