358ad182f4fb461adf79d8f2685e546b02479a01
Martín Silva Molina <msilvamolina@gmail.com>
2024-11-22 07:10:20 +0100

    ya tenemos las notificaciones

--- lib/app/modules/account/notifications_list/views/notifications_list_view.dart
+++ lib/app/modules/account/notifications_list/views/notifications_list_view.dart
@@ -3,6 +3,8 @@ import 'package:flutter_estrellas/app/components/appbar/estrellas_appbar.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../themes/styles/colors.dart';
+import '../../../../themes/styles/typography.dart';
 import '../controllers/notifications_list_controller.dart';
 import '../widgets/notifications_empty_state.dart';
 
@@ -10,11 +12,44 @@ class NotificationsListView extends GetView<NotificationsListController> {
   const NotificationsListView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: EstrellasAppbar(title: 'Notificaciones'),
-      body: Padding(
-        padding: const EdgeInsets.all(26),
-        child: NotificationsEmptyState(),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: EstrellasAppbar(title: 'Notificaciones'),
+        body: Column(
+          children: [
+            PreferredSize(
+              preferredSize: TabBar(
+                tabs: <Widget>[],
+              ).preferredSize,
+              child: Container(
+                color: white,
+                child: TabBar(
+                  labelStyle: TypographyStyle.bodyBlackLarge,
+                  indicatorColor: primaryBase,
+                  labelColor: secondaryBase,
+                  tabs: [
+                    Tab(text: "Todas"),
+                    Tab(text: "Leídas"),
+                    Tab(text: "Sin leer"),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(26),
+                child: TabBarView(
+                  children: [
+                    NotificationsEmptyState(),
+                    NotificationsEmptyState(),
+                    NotificationsEmptyState(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
