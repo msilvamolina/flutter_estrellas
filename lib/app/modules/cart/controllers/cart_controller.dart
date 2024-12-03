0eb3136b93099338a351de9fb95ac125ab9583c8
Martín Silva Molina <msilvamolina@gmail.com>
2024-12-03 15:11:19 +0100

    .

--- lib/app/modules/cart/controllers/cart_controller.dart
+++ lib/app/modules/cart/controllers/cart_controller.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_estrellas/app/app/controllers/main_controller.dart';
 import 'package:get/get.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -23,21 +23,16 @@ void main() async {
   const String flavor = String.fromEnvironment('flavor', defaultValue: '');
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
-  // Mantener el splash screen visible
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  // Inicializar entorno y almacenamiento local
   await Environment.instance.init(env: flavor);
   await GetStorage.init();
 
-  // Inicializar Firebase y Remote Config
   await initFirebase();
   await initRemoteConfig();
 
-  // Inicializar dependencias
   DependecyInjection.init();
 
-  // Manejo global de errores
   runZonedGuarded(
     () {
       runApp(const MyApp());
@@ -48,7 +43,6 @@ void main() async {
   );
 }
 
-/// Inicialización de Firebase
 Future<void> initFirebase() async {
   FirebaseOptions? firebaseOptions;
   if (kIsWeb) {
@@ -77,7 +71,6 @@ Future<void> initFirebase() async {
       storageBucket: firebaseConfig['storageBucket'],
     );
   } else if (Platform.isIOS) {
-    // Usar el archivo GoogleService-Info.plist para iOS
     await Firebase.initializeApp();
     return;
   }
@@ -85,7 +78,6 @@ Future<void> initFirebase() async {
   await Firebase.initializeApp(options: firebaseOptions);
 }
 
-/// Inicialización de Firebase Remote Config
 Future<void> initRemoteConfig() async {
   final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
 
@@ -110,7 +102,6 @@ class MyApp extends StatelessWidget {
     TextTheme textTheme = createTextTheme(context, "Montserrat", "Montserrat");
     MaterialTheme theme = MaterialTheme(textTheme);
 
-    // Quitar el splash screen después de cargar todo
     FlutterNativeSplash.remove();
 
     return GetMaterialApp(
@@ -126,9 +117,7 @@ class MyApp extends StatelessWidget {
       fallbackLocale: const Locale('es'),
       debugShowCheckedModeBanner: false,
       theme: theme.light(),
-      // darkTheme: theme.dark(),
       themeMode: ThemeMode.light,
-      // themeMode: ThemeService.getThemeMode(),
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
       builder: (context, child) => RemoteConfigLayout(child: child!),
