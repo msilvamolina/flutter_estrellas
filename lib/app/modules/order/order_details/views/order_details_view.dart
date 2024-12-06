70bc8d1d69d6839d3cbb97ded5514b788153d40e
Martín Silva Molina <msilvamolina@gmail.com>
2024-12-06 23:31:22 +0100

    new order details

--- lib/app/modules/order/order_details/views/order_details_view.dart
+++ lib/app/modules/order/order_details/views/order_details_view.dart
@@ -2,21 +2,43 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../components/appbar/estrellas_appbar.dart';
+import '../../../../data/models/user_product_cart/user_product_cart_model.dart';
+import '../../../../themes/styles/colors.dart';
 import '../controllers/order_details_controller.dart';
+import '../widgets/order_details_bottom_sheet.dart';
+import '../widgets/order_details_card.dart';
 
 class OrderDetailsView extends GetView<OrderDetailsController> {
   const OrderDetailsView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('OrderDetailsView'),
-        centerTitle: true,
+      appBar: EstrellasAppbar(
+        title: 'ID de venta: ${controller.order.orderId}',
+        backgroundColor: neutral100,
       ),
-      body: const Center(
-        child: Text(
-          'OrderDetailsView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: neutral100,
+      bottomNavigationBar: Container(
+        color: white,
+        child: SafeArea(
+          child: OrderDetailsBottomSheet(
+            orderModel: controller.order,
+          ),
+        ),
+      ),
+      body: ListView.separated(
+        itemCount: controller.order.products.length,
+        itemBuilder: (context, index) {
+          return OrderDetailsCard(
+            userProductCartModel: controller.order.products[index],
+          );
+        },
+        separatorBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: const Divider(
+            color: neutral300,
+          ),
         ),
       ),
     );
--- lib/app/modules/order/orders/widgets/order_card.dart
+++ lib/app/modules/order/orders/widgets/order_card.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_estrellas/app/libraries/icons/icons_font.dart';
 import 'package:flutter_estrellas/app/themes/styles/typography.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 
 import '../../../../data/helpers/currency_helper.dart';
 import '../../../../data/models/order/order_model.dart';
+import '../../../../routes/app_pages.dart';
 import '../../../../themes/styles/colors.dart';
 import '../../../../utils/utils.dart';
 
@@ -20,86 +22,92 @@ class OrderCard extends StatelessWidget {
         CurrencyHelpers.moneyFormat(amount: order.profit, withDecimals: false);
 
     String date = Utils.formatDate(order.createdAt);
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                date,
-                style: TypographyStyle.bodyRegularMedium,
-              ),
-              SizedBox(width: 6),
-              Padding(
-                padding: const EdgeInsets.only(top: 2),
-                child: CircleAvatar(
-                  backgroundColor: neutral700,
-                  radius: 1.5,
+    return GestureDetector(
+      onTap: () {
+        Get.toNamed(Routes.ORDER_DETAILS, arguments: order);
+      },
+      child: Container(
+        color: white,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  date,
+                  style: TypographyStyle.bodyRegularMedium,
                 ),
-              ),
-              SizedBox(width: 4),
-              Text(
-                'ID: ${order.orderId}',
-                style: TypographyStyle.bodyRegularMedium,
-              ),
-            ],
-          ),
-          SizedBox(height: 8),
-          Row(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: info50,
-                    borderRadius: BorderRadius.all(Radius.circular(12))),
-                width: 52,
-                height: 52,
-                child: Center(
-                  child: SvgPicture.asset(
-                    'assets/svg/Scroll.svg',
-                    width: 26,
-                    height: 26,
+                SizedBox(width: 6),
+                Padding(
+                  padding: const EdgeInsets.only(top: 2),
+                  child: CircleAvatar(
+                    backgroundColor: neutral700,
+                    radius: 1.5,
                   ),
                 ),
-              ),
-              SizedBox(width: 12),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    order.status,
-                    style: TypographyStyle.bodyBlackLarge
-                        .copyWith(color: neutral700),
+                SizedBox(width: 4),
+                Text(
+                  'ID: ${order.orderId}',
+                  style: TypographyStyle.bodyRegularMedium,
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: info50,
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
+                  width: 52,
+                  height: 52,
+                  child: Center(
+                    child: SvgPicture.asset(
+                      'assets/svg/Scroll.svg',
+                      width: 26,
+                      height: 26,
+                    ),
                   ),
-                  Text(
-                    '+${order.points} puntos',
-                    style: TypographyStyle.bodyRegularMedium,
-                  ),
-                ],
-              ),
-              Spacer(),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    amountStr,
-                    style: TypographyStyle.bodyBlackLarge
-                        .copyWith(color: neutral700),
-                  ),
-                  Text(
-                    '+${profitStr}',
-                    style: TypographyStyle.bodyRegularMedium,
-                  ),
-                ],
-              ),
-              SizedBox(width: 16),
-              Icon(
-                EstrellasIcons.arrowRight,
-                color: neutral700,
-              ),
-            ],
-          ),
-        ],
+                ),
+                SizedBox(width: 12),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      order.status,
+                      style: TypographyStyle.bodyBlackLarge
+                          .copyWith(color: neutral700),
+                    ),
+                    Text(
+                      '+${order.points} puntos',
+                      style: TypographyStyle.bodyRegularMedium,
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      amountStr,
+                      style: TypographyStyle.bodyBlackLarge
+                          .copyWith(color: neutral700),
+                    ),
+                    Text(
+                      '+${profitStr}',
+                      style: TypographyStyle.bodyRegularMedium,
+                    ),
+                  ],
+                ),
+                SizedBox(width: 16),
+                Icon(
+                  EstrellasIcons.arrowRight,
+                  color: neutral700,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
