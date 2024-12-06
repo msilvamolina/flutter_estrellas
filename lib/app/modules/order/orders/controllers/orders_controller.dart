2dad674ea7c55daad3fd38d7ba670d618ca45ac1
Martín Silva Molina <msilvamolina@gmail.com>
2024-12-06 22:20:25 +0100

    new orders screen

--- lib/app/modules/order/orders/controllers/orders_controller.dart
+++ lib/app/modules/order/orders/controllers/orders_controller.dart
@@ -1,25 +1,45 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../app/controllers/main_controller.dart';
 import '../../../../app/controllers/user_product_controller.dart';
+import '../../../../data/models/order/order_model.dart';
 
 class OrdersController extends GetxController {
   MainController mainController = Get.find<MainController>();
   UserProductController userProductController =
       Get.find<UserProductController>();
 
+  final RxList<OrderModel> _filteredList = <OrderModel>[].obs;
+  final TextEditingController searchController = TextEditingController();
+
+  RxBool showCancelButton = false.obs;
+  List<OrderModel> get filteredList => _filteredList;
+
   @override
   void onInit() {
     super.onInit();
+    _filteredList.assignAll(userProductController.listOrder);
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void filterList(String query) {
+    if (query.isEmpty) {
+      showCancelButton.value = false;
+      _filteredList.assignAll(userProductController.listOrder);
+    } else {
+      showCancelButton.value = true;
+
+      _filteredList.assignAll(
+        userProductController.listOrder
+            .where((item) => item.orderId.toString().contains(query))
+            .toList(),
+      );
+    }
   }
 
   @override
   void onClose() {
+    searchController.dispose();
     super.onClose();
   }
 }
--- lib/app/modules/order/orders/views/orders_view.dart
+++ lib/app/modules/order/orders/views/orders_view.dart
@@ -6,8 +6,10 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import '../../../../routes/app_pages.dart';
 import '../../../../themes/styles/colors.dart';
+import '../../../../themes/styles/typography.dart';
 import '../controllers/orders_controller.dart';
 import '../widgets/order_card.dart';
+import '../widgets/order_search_empty_state.dart';
 import 'orders_empty_state.dart';
 
 class OrdersView extends GetView<OrdersController> {
@@ -28,19 +30,72 @@ class OrdersView extends GetView<OrdersController> {
       ),
       body: Obx(
         () => controller.userProductController.listOrder.isNotEmpty
-            ? ListView.separated(
-                itemCount: controller.userProductController.listOrder.length,
-                itemBuilder: (context, index) {
-                  return OrderCard(
-                    order: controller.userProductController.listOrder[index],
-                  );
-                },
-                separatorBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: const Divider(
-                    color: neutral300,
+            ? Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200], // Fondo gris
+                      borderRadius:
+                          BorderRadius.circular(16), // Bordes redondeados
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: controller.searchController,
+                            onChanged: (value) {
+                              controller.filterList(value);
+                            },
+                            cursorColor: neutral900,
+                            decoration: InputDecoration(
+                              hintText: 'Buscar por ID',
+                              hintStyle: TypographyStyle.bodyRegularMedium
+                                  .copyWith(color: neutral900),
+                              border: InputBorder.none,
+                              prefixIcon: Icon(
+                                EstrellasIcons.search,
+                                color: neutral900,
+                              ),
+                              contentPadding: const EdgeInsets.only(
+                                  top: 11), // Padding solo arriba
+                            ),
+                            style: TypographyStyle.bodyBlackLarge,
+                          ),
+                        ),
+                        if (controller.showCancelButton.value)
+                          IconButton(
+                            icon:
+                                Icon(EstrellasIcons.xCircle, color: neutral900),
+                            onPressed: () {
+                              controller.searchController.clear();
+                              controller.filterList('');
+                            },
+                          ),
+                      ],
+                    ),
                   ),
-                ),
+                  if (controller.filteredList.isNotEmpty)
+                    Expanded(
+                      child: ListView.separated(
+                        itemCount: controller.filteredList.length,
+                        itemBuilder: (context, index) {
+                          return OrderCard(
+                            order: controller.filteredList[index],
+                          );
+                        },
+                        separatorBuilder: (context, index) => Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: const Divider(
+                            color: neutral300,
+                          ),
+                        ),
+                      ),
+                    )
+                  else
+                    OrderSearchEmptyState()
+                ],
               )
             : OrdersEmptyState(),
       ),
