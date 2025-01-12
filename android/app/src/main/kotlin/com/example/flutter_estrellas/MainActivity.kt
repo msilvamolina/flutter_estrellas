c50c2fcd8887c390dc7587b64e234489f979aaaf
Martín Silva Molina <msilvamolina@gmail.com>
2025-01-12 08:26:47 +0100

    ya funciona el login con google

--- android/app/src/main/kotlin/com/example/flutter_estrellas/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_estrellas/MainActivity.kt
@@ -1,4 +1,12 @@
 package app.estrellas.dev
-import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
