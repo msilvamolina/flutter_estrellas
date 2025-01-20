c7a8637a04cbecd0723fb7c824e93be3209ea54b
Martín Silva Molina <msilvamolina@gmail.com>
2025-01-20 12:56:02 +0100

    facebook works in android :D

--- android/app/src/main/kotlin/com/example/flutter_estrellas/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_estrellas/MainActivity.kt
@@ -1,12 +1,6 @@
 package app.estrellas.dev
 
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
+class MainActivity: FlutterActivity() {
 }
